4d4d30b1cd0347b547039c5aa2be6af749c6a25c
Liisa Davydov <liisamuttonen@gmail.com>
2024-09-07 02:43:34 +0300

    Add add book and delete book options

--- bookstore/src/main/java/com/example/bookstore/web/BookController.java
+++ bookstore/src/main/java/com/example/bookstore/web/BookController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.bookstore.domain.Book;
 import com.example.bookstore.repository.BookRepository;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -33,5 +36,24 @@ public class BookController {
         model.addAttribute("books", books);
         return "booklist";
     }
+
+    @GetMapping("/addbook")
+    public String addBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/addbook")
+    public String addBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
+    
+    @GetMapping("/detelebook/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/booklist";
+    }
+    
     
 }
