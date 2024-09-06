84bc2a93cf8b45a5358f39832de543960d647810
Liisa Davydov <liisamuttonen@gmail.com>
2024-09-07 02:26:56 +0300

    add html booklist

--- bookstore/src/main/java/com/example/bookstore/web/BookController.java
+++ bookstore/src/main/java/com/example/bookstore/web/BookController.java
@@ -1,14 +1,37 @@
 package com.example.bookstore.web;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.repository.BookRepository;
+
+import org.springframework.ui.Model;
+
 
 @Controller
 public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
+
     @GetMapping("/index")
     @ResponseBody
     public String index() {
         return "Welcome to the Bookstore!";
     }
+
+    @GetMapping("/booklist")
+    public String booklist(Model model) {
+        List<Book> books = bookRepository.findAll();
+        model.addAttribute("books", books);
+        return "booklist";
+    }
+    
 }
