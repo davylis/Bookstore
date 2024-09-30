5826012f69028c579f8d651d073ddcfb24d6441a
Liisa Davydov <liisamuttonen@gmail.com>
2024-09-30 18:11:05 +0300

    modify securityconfig file

--- bookstore/src/main/java/com/example/bookstore/WebSecurityConfig.java
+++ bookstore/src/main/java/com/example/bookstore/WebSecurityConfig.java
@@ -1,51 +1,64 @@
 package com.example.bookstore;
 
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import com.example.bookstore.web.UserDetailServiceImpl;
+
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
-    @Autowired
-    private UserDetailServiceImpl userDetailsService;
 
-    private static final AntPathRequestMatcher[] WHITE_LIST_URLS = {
-            new AntPathRequestMatcher("/api/books**"),
-            new AntPathRequestMatcher("/h2-console/**") };
+        @Bean
+        public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+                http
+                        .authorizeHttpRequests(authorize -> authorize
+                                        .anyRequest().authenticated()
+                        )
+                                .formLogin(formlogin -> formlogin
+                                .loginPage("/login")
+                                        .defaultSuccessUrl("/booklist", true)
+                                        .permitAll()
+                                )
+                                .logout(logout -> logout
+                                                .permitAll());
 
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+                return http.build();
 
-        http.authorizeHttpRequests(
-                authorize -> authorize
-                        .requestMatchers(antMatcher("/css/**")).permitAll()
-                        .requestMatchers(WHITE_LIST_URLS).permitAll()
-                        .anyRequest().authenticated())
-                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions
-                        .disable())) // for h2console
-                .formLogin(formlogin -> formlogin.loginPage("/login")
-                        .defaultSuccessUrl("/booklist", true)
-                        .permitAll())
-                .logout(logout -> logout.permitAll())
-                .csrf(csrf -> csrf.disable()); // not for production, just for development
+        }
 
-        return http.build();
+        @Bean
+        public UserDetailsService userDetailsService() {
+                List<UserDetails> users = new ArrayList<UserDetails>();
+                PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
-    }
+                UserDetails user1 = User.withUsername("user")
+                        .password(passwordEncoder.encode("user"))
+                        .roles("USER")
+                        .build();  
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-    }
+                UserDetails user2 = User.withUsername("admin")
+                        .password(passwordEncoder.encode("admin"))
+                        .roles("USER", "ADMIN")
+                        .build();
+
+                users.add(user1);
+                users.add(user2);
+
+                return new InMemoryUserDetailsManager(users);
+
+        }
 }
