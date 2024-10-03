3a94b6b43c20bb1bb4a26547b3725bd833786f1f
Liisa Davydov <liisamuttonen@gmail.com>
2024-10-03 15:20:04 +0300

    Create users and save and save to userRepository

--- bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -1,7 +1,9 @@
 package com.example.bookstore;
 
+import com.example.bookstore.domain.AppUser;
 import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.Category;
+import com.example.bookstore.repository.AppUserRepository;
 import com.example.bookstore.repository.BookRepository;
 import com.example.bookstore.repository.CategoryRepository;
 
@@ -15,7 +17,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class BookstoreApplication {
 	//is used to output log messages, which can help debugging, monitoring and maintain application
-	@SuppressWarnings("unused")
 	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
@@ -23,7 +24,8 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	CommandLineRunner initDatabase(BookRepository bookRepository, CategoryRepository categoryRepository){
+	CommandLineRunner initDatabase(BookRepository bookRepository, CategoryRepository categoryRepository, 
+			AppUserRepository userRepository){
 		return args -> {
 			//create categories
 			Category fiction = new Category("Fiction");
@@ -34,6 +36,19 @@ public class BookstoreApplication {
 			categoryRepository.save(fiction);
             categoryRepository.save(nonFiction);
 			categoryRepository.save(romance);
+
+			//create users
+			AppUser user1 = new AppUser("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "ROLE_USER", 
+							"user@email.fi");
+			AppUser user2 = new AppUser("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ROLE_ADMIN", 
+							"admin@email.fi");
+			userRepository.save(user1);
+			userRepository.save(user2);
+
+			log.info("fetch all students");
+			for (Book book : bookRepository.findAll()) {
+				log.info(book.toString());
+			}
 
 			//creating books and associating them with categories
 			bookRepository.save(new Book("The Best of Me", "Nicholas Sparks", 2011, "9781455502547", 20.00, romance));
