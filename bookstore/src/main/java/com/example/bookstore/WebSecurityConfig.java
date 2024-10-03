5f446dbc454aaec658ed6b25d27cba9178cfd2a3
Liisa Davydov <liisamuttonen@gmail.com>
2024-10-03 15:27:42 +0300

    Delete user creation and add Hash encrypting

--- bookstore/src/main/java/com/example/bookstore/WebSecurityConfig.java
+++ bookstore/src/main/java/com/example/bookstore/WebSecurityConfig.java
@@ -1,20 +1,15 @@
 package com.example.bookstore;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.bookstore.web.UserDetailServiceImpl;
 
 
 
@@ -22,43 +17,34 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
 
+        @Autowired
+                private UserDetailServiceImpl userDetailsService;
+
         @Bean
         public SecurityFilterChain configure(HttpSecurity http) throws Exception {
                 http
                         .authorizeHttpRequests(authorize -> authorize
-                                        .anyRequest().authenticated()
+                                        .requestMatchers( "/css/**").permitAll()
+                                        .anyRequest().authenticated() //Require authentication
+                                       
                         )
                                 .formLogin(formlogin -> formlogin
                                 .loginPage("/login")
                                         .defaultSuccessUrl("/booklist", true)
-                                        .permitAll()
+                                        .permitAll() //allow all to see login page
+                                        
                                 )
                                 .logout(logout -> logout
-                                                .permitAll());
+                                                .permitAll() //allow everyonw to see logout page
+                            );
 
                 return http.build();
 
         }
 
-        @Bean
-        public UserDetailsService userDetailsService() {
-                List<UserDetails> users = new ArrayList<UserDetails>();
-                PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
-                UserDetails user1 = User.withUsername("user")
-                        .password(passwordEncoder.encode("user"))
-                        .roles("USER")
-                        .build();  
-
-                UserDetails user2 = User.withUsername("admin")
-                        .password(passwordEncoder.encode("admin"))
-                        .roles("USER", "ADMIN")
-                        .build();
-
-                users.add(user1);
-                users.add(user2);
-
-                return new InMemoryUserDetailsManager(users);
-
+         @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+                auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
         }
+    
 }
