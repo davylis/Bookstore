a48e14a96563496d09830e30cf1e0f9f4072641f
Liisa Davydov <liisamuttonen@gmail.com>
2024-10-07 10:02:22 +0300

    Modify crepository

--- bookstore/src/main/java/com/example/bookstore/repository/CategoryRepository.java
+++ bookstore/src/main/java/com/example/bookstore/repository/CategoryRepository.java
@@ -2,7 +2,9 @@ package com.example.bookstore.repository;
 
 import com.example.bookstore.domain.Category;
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    List<Category> findByName(String name);
 }
 
